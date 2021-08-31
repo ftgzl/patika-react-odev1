0e6a2b55889b23b18674555b0357f0970b33bc7b
fuatguzel <fuatguzel754@gmail.com>
2021-08-31 20:48:47 +0300

    Completed.

---
+++ src/lib/service.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export default async function getData(userId) {
+  const {data} = await axios(`https://jsonplaceholder.typicode.com/users/${userId}`)
+ console.log(data);
+}
+
+
